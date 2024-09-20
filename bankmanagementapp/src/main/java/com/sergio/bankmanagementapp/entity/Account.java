4ec4a561890b01df561eb84dd9702558806f2034
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-20 16:26:09 -0500

    Creando la entidad cuenta.

---
+++ bankmanagementapp/src/main/java/com/sergio/bankmanagementapp/entity/Account.java
@@ -0,0 +1,22 @@
+package com.sergio.bankmanagementapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long account_number;
+    @Column
+    private String account_holder_name;
+    @Column
+    private Double account_balance;
+}
