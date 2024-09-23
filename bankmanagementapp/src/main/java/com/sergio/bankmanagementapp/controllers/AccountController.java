37c4d6e6788ced4afeff3b821a7dcb0dad25cab2
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-23 11:55:48 -0500

    Controlar

---
+++ bankmanagementapp/src/main/java/com/sergio/bankmanagementapp/controllers/AccountController.java
@@ -0,0 +1,52 @@
+package com.sergio.bankmanagementapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.sergio.bankmanagementapp.entity.Account;
+import com.sergio.bankmanagementapp.service.AccountService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+    // EN el controlador es en realidad donde se crean las APIS
+
+
+    @Autowired
+    AccountService accountService;
+
+    // Create the account
+    @PostMapping("/create")
+    public ResponseEntity<Account> createAccount(@RequestBody Account account){
+        Account createAccount = accountService.createAccount(account);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createAccount);
+    }
+
+    // Obteniendo los detalles de la cuenta, es decir, de una en especifico:
+    @GetMapping("/{accountNumber}")
+    public Account getAccountByAccountNumber(@PathVariable Long accountNumber) {
+        Account account = accountService.getAccountDetailsByAccountNumber(accountNumber);
+        return account;
+    }
+
+    @GetMapping("/getallaccounts")
+    public List<Account> getAllAccountDetails() {
+        return accountService.getAllAccountDetails();
+    }
+
+    @PutMapping("/deposit/{accountNumber}/{amount}")
+    public Account depositAmount(@PathVariable Long accountNumber,@PathVariable Double amount){
+        return accountService.depositAmount(accountNumber, amount);
+    }
+
+    @PutMapping("/withdraw/{accountNumber}/{amount}")
+    public Account withDrawAmount(@PathVariable Long accountNumber, @PathVariable Double amount){
+        return accountService.withDrawAmount(accountNumber, amount);
+    }
+
+}
